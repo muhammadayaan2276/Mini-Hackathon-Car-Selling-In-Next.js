1810f294587f1f4588a266a6d43ed00c3c732c4f
Ayaan Zeeshan <muhammad.ayaan.zeeshan123@gmail.com>
2024-11-17 00:13:03 +0500

    Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,23 +1,28 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <footer className="bg-gray-100 py-6">
       <div className="container mx-auto flex flex-col items-center space-y-4">
+        {/* Title */}
         <h1 className="text-3xl font-bold font-serif text-red-700 hover:text-red-500">
-          Ayaan's Auto Hub
+          Ayaan&apos;s Auto Hub
         </h1>
 
+        {/* Social Links */}
         <div className="flex space-x-10">
           <a
             href="https://www.facebook.com/profile.php?id=61569038724612"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img
+            <Image
               src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"
               alt="Facebook"
-              className="w-14 h-14 hover:opacity-75"
+              width={56}
+              height={56}
+              className="hover:opacity-75"
             />
           </a>
           <a
@@ -25,23 +30,35 @@ export default function Footer() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img
+            <Image
               src="https://upload.wikimedia.org/wikipedia/commons/e/e9/Linkedin_icon.svg"
               alt="LinkedIn"
-              className="w-14 h-14 hover:opacity-75"
+              width={56}
+              height={56}
+              className="hover:opacity-75"
             />
           </a>
         </div>
 
+        {/* Navigation Links */}
         <nav className="flex flex-wrap justify-center space-x-4 text-md">
-          <div className="hover:underline">About Us</div>
-          <div className="hover:underline">Services</div>
-          <div className="hover:underline">Contact</div>
-          <div className="hover:underline">Privacy Policy</div>
+          <Link href="/about">
+            <a className="hover:underline">About Us</a>
+          </Link>
+          <Link href="/services">
+            <a className="hover:underline">Services</a>
+          </Link>
+          <Link href="/contact">
+            <a className="hover:underline">Contact</a>
+          </Link>
+          <Link href="/privacy-policy">
+            <a className="hover:underline">Privacy Policy</a>
+          </Link>
         </nav>
 
+        {/* Footer Text */}
         <p className="text-lg text-purple-800 hover:text-purple-500">
-          &copy; 2024 Ayaan's Auto Hub. All rights reserved.
+          &copy; 2024 Ayaan&apos;s Auto Hub. All rights reserved.
         </p>
       </div>
     </footer>
